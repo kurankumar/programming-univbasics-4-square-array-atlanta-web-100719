aa1cdd7266384c0d70aedead8b7cfeeffda86d4a
Kuran V Kumar <kurankumar@gmail.com>
2019-09-25 20:00:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,13 @@
 def square_array(array)
+  
   squared_numbs = []
   counter = 0
+  
   while counter < array.length do
     squared_numbs << (array[counter] ** 2)
     counter = counter + 1
+  end
+  
+  square_array
+  
 end
